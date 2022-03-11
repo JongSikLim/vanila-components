8cd25b73c61c25731e5427de3473c5d09429d65b
임종식 <whdtlr1218@gmail.com>
2022-03-11 17:03:51 +0900

    [feature] - 프로젝트 설정

---
+++ src/components/NewComponent.js
@@ -0,0 +1,19 @@
+export default function NewComponent({ $target, initialState }) {
+    this.state = {
+        ...initialState
+    }
+    this.setState = nextState => {
+        this.state = nextState;
+        this.render();
+    }
+
+    const $NewComponent = document.createElement('document');
+    $target.appendChild($NewComponent);
+
+    this.render = () => {
+        const { } = this.state
+        $NewComponent.innerHTML = ``;
+    }
+
+    this.render();
+}
